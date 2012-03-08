3ebd9816eae1cacdff8f8122204080dd160bdb15
Silas Barta <sbarta@gmail.com>
2012-03-07 18:56:29 -0800

    added list/task creation functionality

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
 class TasksController < ApplicationController
+  def new
+    @task = Task.new
+  end
+  
+  def create
+    @task = Task.new(params[:task])
+    @task.list_id = current_user.id
+    if @task.save
+      flash[:success] = "New task added"
+      redirect_to root_path
+    else
+      flash[:error] = "Task creation failed."
+      render 'new'
+    end
+  end
 end
