f9f0166cbe7b1bfe4cacb75b2cf75535804f2e12
Mehul Kar <mehul.kar@gmail.com>
2012-03-08 15:27:38 -0800

    Priority numbering

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,8 @@ class List < ActiveRecord::Base
   has_many :tasks, :dependent => :destroy
   belongs_to :user
   validates_presence_of :user_id
+  
+  def priority_sort
+    self.tasks.sort_by {|task| task.priority_number} 
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,13 +1,33 @@
 class Task < ActiveRecord::Base
+  attr_accessor :all_tasks
   belongs_to :list
   validates :list_id, :description, :presence => true
   validates :priority_number, :numericality => { :only_integer => true }
+  before_save :check_priority_number_uniqueness, :priority_number_increment
   
   def report_status
     if completion_status == true
       "Done"
     else
       "Incomplete"
+    end
+  end
+  
+  def priority_number_increment
+    @all_tasks.each do |task| 
+      if task.priority_number >= @equal_priority_number ||= (@all_tasks.length + 1)
+        task.priority_number += 1
+        task.save
+      end
+    end  
+  end
+  
+  def check_priority_number_uniqueness
+    @all_tasks = Task.find_all_by_list_id(self.list_id)
+    @all_tasks.each do |task|
+      if self.priority_number == task.priority_number
+        @equal_priority_number = task.priority_number
+      end
     end
   end
   
