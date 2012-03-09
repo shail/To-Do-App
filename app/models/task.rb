7032cc061660fb8bdb24b2b723aaf432d4677e67
Mehul Kar <mehul.kar@gmail.com>
2012-03-08 17:27:13 -0800

    Able to sort by priority number

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,7 +5,7 @@ class Task < ActiveRecord::Base
   validates :priority_number, :numericality => { :only_integer => true }
   validates :priority_level, :presence => true
   validates :due_date, :presence => true
-  # before_save :check_priority_number_uniqueness, :priority_number_increment
+
  
   def report_status
     if completion_status == true
@@ -14,26 +14,5 @@ class Task < ActiveRecord::Base
       "Incomplete"
     end
   end
-  
-
-  #BUGBUG Illegal instruction error, server shuts down. Can't double task priority number that isn't the   last one
-  
-  # def priority_number_increment
-  #   @all_tasks.each do |task| 
-  #     if task.priority_number >= (@equal_priority_number ||= (@all_tasks.length + 1))
-  #       task.priority_number += 1
-  #       task.save
-  #     end
-  #   end  
-  # end
-  # 
-  # def check_priority_number_uniqueness
-  #   @all_tasks = Task.find_all_by_list_id(self.list_id)
-  #   @all_tasks.each do |task|
-  #     if self.priority_number == task.priority_number
-  #       @equal_priority_number = task.priority_number
-  #     end
-  #   end
-  # end
   
 end
