5207546b72e988d9ff3fb365ec45a7f3af761672
Mehul Kar <mehul.kar@gmail.com>
2012-03-08 16:31:40 -0800

    Priority Level set up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120308204913) do
+ActiveRecord::Schema.define(:version => 20120309000339) do
 
   create_table "lists", :force => true do |t|
     t.integer  "user_id"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(:version => 20120308204913) do
     t.datetime "created_at",        :null => false
     t.datetime "updated_at",        :null => false
     t.integer  "priority_number"
+    t.string   "priority_level"
   end
 
   create_table "users", :force => true do |t|
